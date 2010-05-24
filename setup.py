531c479b3e9199c68ad657ba4cf35a71f9e694bd
shaunsephton <shaunsephton@gmail.com>
2010-05-24 12:24:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-contact',
+    version='dev',
+    description='Django contact app.',
+    author='Praekelt Consulting',
+    author_email='dev@praekelt.com',
+    url='https://github.com/praekelt/django-contact',
+    packages = find_packages(),
+    include_package_data=True,
+)
