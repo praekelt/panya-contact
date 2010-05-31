425544ea1190a15b7893f640e629708ff1783b99
qoda <jonathan@blu-marble.co.za>
2010-05-31 14:21:51 +0200

    removed recapcha field

--- contact/forms.py
+++ contact/forms.py
@@ -1,39 +1,22 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    firstname = forms.CharField(
-        max_length=100,
+    name = forms.CharField(
+        max_length=150,
         label='Name:',
-        help_text="First Name and Last.",
-        widget=forms.TextInput(attrs={'class':'required'}),
+        widget=forms.TextInput(attrs={'class':'required shift'}),
         error_messages={'required': 'Please enter your name.'}
     )
-    lastname = forms.CharField(
-        max_length=100,
-        label='',
-        widget=forms.TextInput(attrs={'class':'required'}),
-        error_messages={'required': 'Please enter your surname.'}
-    )
     email_address = forms.EmailField(
-        max_length=100,
+        max_length=150,
         label='Email:',
-        help_text='We will send you account notices here - so make sure its good.',
-        widget=forms.TextInput(attrs={'class':'required email'}),
+        widget=forms.TextInput(attrs={'class':'required email shift'}),
         error_messages={'required': 'Please enter your email address.'}
     )
-    '''
-    recaptcha_response_field = forms.CharField(
-        max_length=100,
-        label='Human?',
-        help_text='Spam prevention code. Enter the words above.',
-        widget=forms.TextInput(attrs={'class':'required', 'id': 'recaptcha_response_field', 'autocomplete': 'off'}),
-        error_messages={'required': 'Please enter the words above.'}
-    )
-    '''
     message = forms.CharField(
         max_length=10000,
         label='Message:',
-        widget=forms.Textarea(),
+        widget=forms.Textarea(attrs={'class':'required shift'}),
         error_messages={'required':'Please enter a message.'}
     )
         
@@ -46,22 +29,6 @@ class ContactForm(forms.Form):
                 self._errors['recaptcha_response_field'] = ['Incorrect, please try again.',]
                 valid = False
         return valid
-    
-    def get_recipients(self):
-        recipients = []
-        
-        settings = Settings.objects.all()
-        if settings:
-            contact_email_recipients = settings[0].contact_email_recipients
-            if contact_email_recipients:
-                split_recipients = [recipient.replace('\r', '') for recipient in contact_email_recipients.split('\n')]
-                for recipient in split_recipients:
-                    if recipient:
-                        recipients.append(recipient)
-
-        if not recipients:
-            mail_managers('Error: No email address specified', 'Users are trying to contact style Intern for which no contact email recipients are specified.', fail_silently=False)
-        return recipients
     
     def send_message(self, recipients):
         
